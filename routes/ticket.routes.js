b58fea8cbc27f04510e894b568772afa2a0fc906
Sujal-s-patil <153197090+Sujal-s-patil@users.noreply.github.com>
2025-03-09 11:27:59 +0530

    Update ticket.routes.js

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const ticketRouter = express.Router();
-const { ticketRecords, createTicket ,specificTicket, lastComplaintId} = require("../controllers/ticket.controller.js")
+const { ticketRecords, createTicket ,specificTicket, lastComplaintId , ticketStatus} = require("../controllers/ticket.controller.js")
 
 
 ticketRouter.get("/get", ticketRecords);
 ticketRouter.post("/specific",specificTicket);
 ticketRouter.post("/create", createTicket);
 ticketRouter.get("/last", lastComplaintId);
-
+ticketRouter.put("/status", ticketStatus);
 
 
 module.exports = ticketRouter;
